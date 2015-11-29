569bd800b221da6aaf4c1e14e8745343fdca3a5e
Alex Lu <op8867555@gmail.com>
2015-11-29 17:59:59 +0800

    改寫 Main 使用 optparse-applicative 來處理參數

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE NamedFieldPuns #-}
 module Main where
 
 import Trans
@@ -5,15 +6,43 @@ import System.IO (getContents)
 import System.Environment (getArgs)
 import Data.Aeson (encode, toJSON)
 import qualified Data.ByteString.Lazy.Char8 as BS
+import Data.List (intercalate)
+import Data.Maybe (catMaybes)
+import Options.Applicative
+
+data OutputLevel = Ast | Expr | Json
+                 deriving (Show, Read)
+
+data Options =
+    Options { file :: Maybe String
+            , outputLevel :: OutputLevel
+            } deriving (Show)
+
+
+parser :: Parser Options
+parser =
+    Options
+        <$> optional (strOption (long "file"
+                                 <> metavar "FILE")
+                     <|> (argument str (metavar "FILE")))
+        <*> (option auto (long "output-level"
+                          <> help "output-level: Ast, Expr, Json")
+             <|> pure Json)
 
 main :: IO ()
-main = do
-    args <- getArgs
-    (name, input) <- case args of
-                       [] -> (,) <$> return "<interact>" <*> getContents
-                       (x:[]) -> (,) <$> return x <*> readFile x
-    bindings <- transpile name input :: IO [(TName, Expr Int)]
-    let jsonBinds = map toJSON bindings
-    BS.putStrLn . encode $ toJSON jsonBinds
+main =
+    do
+        Options {file, outputLevel} <- execParser opts
+        (name, input) <- case file of
+                           Nothing -> (,) <$> return "<interact>" <*> getContents
+                           Just x -> (,) <$> return x <*> readFile x
+        bindings <- transpile name input :: IO [(TName, Expr ())]
+        case outputLevel of
+          Json -> BS.putStrLn . encode . toJSON $ map toJSON bindings
+          Expr -> mapM_ print bindings
+          _ -> error "not implemented yet"
 
-
+    where opts = info (helper <*> parser)
+                    (fullDesc
+                    <> progDesc "transforming Haskell programs to lambda calculus programs"
+                    <> header "Transpiler")
