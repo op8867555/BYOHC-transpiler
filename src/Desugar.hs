df2d1268dc5b6d48c565d6c4dd917a16fdb07d45
Alex Lu <op8867555@gmail.com>
2016-02-09 00:21:01 +0800

    實作 List / String 與其 pattern 的 desugar

--- src/Desugar.hs
+++ src/Desugar.hs
@@ -6,6 +6,7 @@ where
 
 import Desugar.Monad
 import Desugar.If (desugarIf)
+import Desugar.Lit (desugarLit)
 import Desugar.Where (desugarWhere)
 import Desugar.Rename (rename)
 import Desugar.GADT (desugarGADT)
@@ -13,6 +14,7 @@ import Desugar.Case (desugarCase)
 import Control.Monad
 
 desugar = desugarIf
+      >=> desugarLit
       >=> desugarWhere
       >=> rename
       >=> desugarGADT
