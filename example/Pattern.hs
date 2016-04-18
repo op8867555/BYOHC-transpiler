e3007202062341a1976cea04cce7b735fdaa4a12
Alex Lu <op8867555@gmail.com>
2016-04-18 21:54:56 +0800

    新增 Irrefutable Pattern 的簡單範例

--- example/Pattern.hs
+++ example/Pattern.hs
@@ -5,10 +5,19 @@ data Maybe :: * -> * where
 data Pair where
      Pair :: a -> b -> Pair a b
 
+undefined = undefined
+
 t = \x -> case x of
                Just Nothing -> "Just Nothing"
                Just (Just (Pair a 0)) -> a
                Nothing -> "Nothing"
+
+-- `irrefutable undefined` should return "hi"
+irrefutable = \x -> case x of
+                        ~(Pair a b) -> "hi"
+-- `nonIrrefutable undefined` should fail
+nonIrrefutable = \x -> case x of
+                            (Pair a b) -> "fail anyway"
 
 
 main = putStrLn (t (Just (Just (Pair "hi" 0))))
